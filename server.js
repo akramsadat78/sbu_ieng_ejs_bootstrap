a4d57d3195133ccc5d0b961d8bf5e84e139329ce
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-26 16:28:44 +0430

    add api get and take json file from http://covid19api.xapix.io/v2/locations

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const fs = require('fs');
+
 var json = require('./data.json');
 
+var inside = require('point-in-polygon');
+
+console.log(1);
 // Set EJS as templating engine 
 app.set('view engine', 'ejs');
 
@@ -15,6 +24,23 @@ app.get('/', (req, res) => {
 
     res.render('h', { data: data });
 
+});
+console.log(2);
+
+app.put('/', function(req, res) {
+
+    let data = req.body;
+
+    var atest_inf_for_all_countries = {
+        "latest": [...json.latest, data],
+        "locations": [...json.locations, data]
+    }
+
+    data = JSON.stringify(atest_inf_for_all_countries, null, 2);
+
+    fs.writeFileSync('data.json', data);
+
+    res.send('Got a PUT request at /user');
 });
 
 var server = app.listen(4000, function() {
