3e2a0534117522a7c74111ade2b57086b70b801d
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-26 14:26:47 +0430

    chnge server content

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-یییی
+var express = require('express');
+var app = express();
+
+// Set EJS as templating engine 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+    res.render('h', { name: 'Akashdeep' });
+
+});
+
+var server = app.listen(4000, function() {
+    console.log('listining to port 4000')
+});
