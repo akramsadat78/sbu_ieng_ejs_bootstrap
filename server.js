4a47c82f37123d932fdb7abc5f0cc3cff43e71c7
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-04-27 11:22:23 +0430

    put axsios in get api

--- server.js
+++ server.js
@@ -3,33 +3,40 @@ var app = express();
 
 const axios = require('axios');
 
-var jsonContent;
 
-axios.get('http://covid19api.xapix.io/v2/locations')
-    .then((response) => {
+
+app.get('/', (req, res) => {
+    let jsonContent;
+    axios.get('http://covid19api.xapix.io/v2/locations')
+        .then(function(response, jsonContent) {
+
             jsonContent = response.data;
+        });
+    console.log(jsonContent.locations[0]);
 
-            var locationsValue = {
-                name: 'Armenia',
-                population: '2951776',
-                confirmedCases: '1677',
-                deaths: '28',
-                recovered: '0'
-            }
+    var locationsValue = {
+        name: 'Armenia',
+        population: '2951776',
+        confirmedCases: '1677',
+        deaths: '28',
+        recovered: '0'
+    }
 
-            var arrObject = [];
-            for (var i = 0; i < jsonContent.locations.length; i++) {
-                arrObject.push({
-                    name: jsonContent.locations[i].country,
-                    population: jsonContent.locations[i].country_population,
-                    confirmedCases: jsonContent.locations[i].latest.confirmed,
-                    deaths: jsonContent.locations[i].latest.deaths,
-                    recovered: jsonContent.locations[i].latest.recovered
-                });
-            }
+    var arrObject = [];
+    for (var i = 0; i < jsonContent.locations.length; i++) {
+        arrObject.push({
+            name: jsonContent.locations[i].country,
+            population: jsonContent.locations[i].country_population,
+            confirmedCases: jsonContent.locations[i].latest.confirmed,
+            deaths: jsonContent.locations[i].latest.deaths,
+            recovered: jsonContent.locations[i].latest.recovered
+        });
+    }
 
-            res.render('index', { dat: arrObject })
+    response.render('index', { dat: arrObject })
+});
 
-        }
 
-    )
+var server = app.listen(4000, function() {
+    console.log('listining to port 4000')
+});
